a7227c46244a61a9c102f5179f60f8378ccaa957
Erasmo Jr <erasmo.cjunior@gmail.com>
2021-03-05 20:35:54 -0300

    Updates

--- main.py
+++ main.py
@@ -4,24 +4,4 @@ Teste Ping-Ping
 caractere ":" como único parâmentro, e retorne "Jogador A" ou
 "Jogador B", dependendo de quem é a vez de sacar.
 """
-import time
-
-def pausa():
-    time.sleep(0.3)
-    return None
-
-def saque():
-    a = 0
-    b = 0
-    for a in range(1, 21):
-        pausa()
-        print("A", a, ":", b,  "B")
-        if a > b:
-            b += 1
-            pausa()
-            print("A", a, ":", b,  "B")
-
-            
-saque()
-
 
